6bb0ce35f4f40af4c9ac54c93ec2c7e76a1c8ddf
IsadinovDias312 <112857129+IsadinovDias312@users.noreply.github.com>
2023-04-04 21:18:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1182463770 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    semianswer = 0
+    for i in range(len(x)):
+      semianswer += x[i]
+    semianswer /= len(x)
+    answer = semianswer/36
+    return answer # Ваш ответ
